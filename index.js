e39fd71c6af944813912dd01695dc367b3ebe3c4
watashidegit <karina.yongr@gmail.com>
2025-04-08 23:33:13 -0400

    trailing netlify_url

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(
     cors({
       credentials: true,
-      origin: process.env.NETLIFY_URL || "http://localhost:5173",
+      origin: process.env.NETLIFY_URL?.replace(/\/$/, "") || "http://localhost:5173",
     })
 );  
 const sessionOptions = {
