a5e050455616285b4f9ab0f270f8ed7fc8c8fdfc
watashidegit <karina.yongr@gmail.com>
2025-04-21 15:04:28 -0400

    add back replace

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(
     cors({
       credentials: true,
-      origin: process.env.NETLIFY_URL || "http://localhost:5173",
+      origin: process.env.NETLIFY_URL.replace(/\/$/, "") || "http://localhost:5173",
     })
 );  
 console.log("Mongo URI:", process.env.MONGO_CONNECTION_STRING);
